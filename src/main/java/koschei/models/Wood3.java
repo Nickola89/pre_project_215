ce0a3d0a22c6ca0b63f9dedffdc234b25dfb52c7
Nickolay Artemyev <Artemyev1000@gmail.com>
2023-05-25 22:00:47 +0900

    Correcting 2

--- src/main/java/koschei/models/Wood3.java
+++ src/main/java/koschei/models/Wood3.java
@@ -5,15 +5,15 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Wood3 {
-    private Rabbit4 rabbit;
+    private final Rabbit4 rabbit;
+
+    public Wood3(Rabbit4 rabbit) {
+        this.rabbit = rabbit;
+    }
 
     @Override
     public String toString() {
         return ", на дереве заяц " + rabbit.toString();
     }
 
-    @Autowired
-    public void setRabbit(Rabbit4 rabbit) {
-        this.rabbit = rabbit;
-    }
 }
